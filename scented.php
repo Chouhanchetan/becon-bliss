f85ea207718c4941d76dc60567dee2310281e4e3
Chouhanchetan <chouhanchetan114@gmail.com>
2024-07-31 16:05:22 +0530

    Add files via upload

---
+++ scented.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scented Candel-Becon Bliss</title>
+    <link rel="stylesheet" href="scented.css">
+</head>
+<body>
+<nav>
+        <ul class="links">
+            <li class="link"><a href="#"></a><b>HOME</b></li>
+            <li class="link"><a href="#"></a><b>SHOP</b></li>
+            <img src="D:\Downloads\logo.png" class="hello" >
+            <li class="link"><a href="#"></a><b>KIND WORDS</b></li>
+            <li class="link"><a href="#"></a><b>CONTACT</b></li>
+        </ul>
+    </nav><br>
+    <center><hr class="hr"></center>
+    <div class="container">
+    <h1>Scented Candels</h1>
+    <p>Welcome to our handcrafted and vegan home fragrance collection! Indulge your senses with our exquisite range of soy wax and coconut-based creations. Each product is carefully handcrafted with love and attention to detail, ensuring a luxurious experience for your mind, body, and soul.</p>
+    </div>
+    <table>
+        <tr>
+            <td class="list"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNLX2G7Ub06Pzmbc7S9VDNB-Dy7s-fmntwYQ&s" alt="">Winter Wonderland Wax Melt<br>$399<br><button>Shop</button></td>
+            <td class="list"><img src="https://i.etsystatic.com/11449848/r/il/ac51c6/1890178840/il_1080xN.1890178840_rd0q.jpg" alt="">Diffuser Set<br>$499<br><button>Shop</button></td>
+            <td class="list"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTvwp5etqaUZk87tk8dKGMvBSMMtyigQh2QLafaXLSKk9BFrSaPHq8clPjh8DrAgv_t-0&usqp=CAU" alt="">Wooden Wick Candle<br>$599<br><button>Shop</button></td>
+            <td class="list"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsE1KblvVIT84veI5BZJ_so_BV_XjWDjtofagtrE7MZYk5RkqHymF4y-lQ9s9hhla9Mag&usqp=CAU" alt="">Dew Drops Candle<br>$799<br><button>Shop</button></td>
+        </tr><br>
+        <tr>
+            <td class="list"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEEhreKj83dOcmrkYoBAN1A5GBdvvWmBZ5ku4cMOW50PLZ7LCz9S0tvFo9-odj0qWL4nE&usqp=CAU" alt="">Citrus Energy Wax Candle<br>$999<br><button>Shop</button> </td>
+            <td class="list"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcAdWLRDL7lfoZoBbyAOh81kO5PtyWIZ3kklv3AKpXADD1fhE8YrdqDhSQUwL6D5puPVY&usqp=CAU" alt="">Far East Votive Candle<br>$999<br><button>Shop</button></td>
+            <td class="list"><img src="https://usercontent.one/wp/mariannetaylor.co.uk/wp-content/uploads/2021/08/Colourful-pretty-content-creation-for-Rare-Lumiere-candles.-Styled-beauty-product-stills-photography-by-Marianne-Taylor._0003.jpg?media=1637181377" alt="">Summer Candy votive Candle<br>$499<br><button>Shop</button></td>
+            <td class="list"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIxhO-CPReG2VEAy_0-WZqWRjAjkynAU6Jlg&s" alt="">Velvet Bloom<br>$499<br><button>Shop</button></td>
+        </tr>
+    </table>
+</bShop
