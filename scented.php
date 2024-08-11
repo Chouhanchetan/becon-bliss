c8f91c734fee2c19eab57f32d935fa739cefc57b
Chouhanchetan <chouhanchetan114@gmail.com>
2024-08-11 13:38:27 +0530

    Update scented.php

--- scented.php
+++ scented.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Scented Candel-Becon Bliss</title>
+    <title>Scented Candel-Beacon Bliss</title>
     <link rel="stylesheet" href="scented.css">
 </head>
 <body>
@@ -18,14 +18,17 @@ include('nav.html');
     </div>
 
 <div class="hello1">
-<div class="list"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNLX2G7Ub06Pzmbc7S9VDNB-Dy7s-fmntwYQ&s" alt="">Winter Wonderland Wax Melt<br>$399<br><button>Shop</button></div>
-<div class="list"><img src="https://i.etsystatic.com/11449848/r/il/ac51c6/1890178840/il_1080xN.1890178840_rd0q.jpg" alt="">Diffuser Set<br>$499<br><button>Shop</button></div>
-<div class="list"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTvwp5etqaUZk87tk8dKGMvBSMMtyigQh2QLafaXLSKk9BFrSaPHq8clPjh8DrAgv_t-0&usqp=CAU" alt="">Wooden Wick Candle<br>$599<br><button>Shop</button></div>
-<div class="list"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsE1KblvVIT84veI5BZJ_so_BV_XjWDjtofagtrE7MZYk5RkqHymF4y-lQ9s9hhla9Mag&usqp=CAU" alt="">Dew Drops Candle<br>$799<br><button>Shop</button></div>
+<div class="list"><a href="winter.php"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNLX2G7Ub06Pzmbc7S9VDNB-Dy7s-fmntwYQ&s" alt=""></a>Winter Wonderland Wax Melt<br>Rs.399<br><a href="winter.php"><button>Shop</button></a></div>
+<div class="list"><a href="diffuser.php"><img src="https://i.etsystatic.com/11449848/r/il/ac51c6/1890178840/il_1080xN.1890178840_rd0q.jpg" alt=""></a>Diffuser Set<br>Rs.499<br><a href="diffuser.php"><button>Shop</button></a></div>
+<div class="list"><a href="wooden.php"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTvwp5etqaUZk87tk8dKGMvBSMMtyigQh2QLafaXLSKk9BFrSaPHq8clPjh8DrAgv_t-0&usqp=CAU" alt=""></a>Wooden Wick Candle<br>Rs.599<br><a href="wooden.php"><button>Shop</button></a></div>
+<div class="list"><a href="dewdrop.php"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsE1KblvVIT84veI5BZJ_so_BV_XjWDjtofagtrE7MZYk5RkqHymF4y-lQ9s9hhla9Mag&usqp=CAU" alt=""></a>Dew Drops Candle<br>Rs.799<br><a href="dewdrop.php"><button>Shop</button></a></div>
 </div>
 <div class="hello1">
-<div class="list"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEEhreKj83dOcmrkYoBAN1A5GBdvvWmBZ5ku4cMOW50PLZ7LCz9S0tvFo9-odj0qWL4nE&usqp=CAU" alt="">Citrus Energy Wax Candle<br>$999<br><button>Shop</button> </div>
-<div class="list"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcAdWLRDL7lfoZoBbyAOh81kO5PtyWIZ3kklv3AKpXADD1fhE8YrdqDhSQUwL6D5puPVY&usqp=CAU" alt="">Far East Votive Candle<br>$999<br><button>Shop</button></div>
-<div class="list"><img src="https://usercontent.one/wp/mariannetaylor.co.uk/wp-content/uploads/2021/08/Colourful-pretty-content-creation-for-Rare-Lumiere-candles.-Styled-beauty-product-stills-photography-by-Marianne-Taylor._0003.jpg?media=1637181377" alt="">Summer Candy votive Candle<br>$499<br><button>Shop</button></div>
-<div class="list"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIxhO-CPReG2VEAy_0-WZqWRjAjkynAU6Jlg&s" alt="">Velvet Bloom<br>$499<br><button>Shop</button></div>
+<div class="list"><a href="citrus.php"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEEhreKj83dOcmrkYoBAN1A5GBdvvWmBZ5ku4cMOW50PLZ7LCz9S0tvFo9-odj0qWL4nE&usqp=CAU" alt=""></a>Citrus Energy Wax Candle<br>Rs.999<br><a href="citrus.php"><button>Shop</button></a></div>
+<div class="list"><a href="far.php"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcAdWLRDL7lfoZoBbyAOh81kO5PtyWIZ3kklv3AKpXADD1fhE8YrdqDhSQUwL6D5puPVY&usqp=CAU" alt=""></a>Far East Votive Candle<br>Rs.999<br><a href="far.php"><button>Shop</button></a></div>
+<div class="list"><a href="summer.php"><img src="https://usercontent.one/wp/mariannetaylor.co.uk/wp-content/uploads/2021/08/Colourful-pretty-content-creation-for-Rare-Lumiere-candles.-Styled-beauty-product-stills-photography-by-Marianne-Taylor._0003.jpg?media=1637181377" alt=""></a>Summer Candy votive Candle<br>Rs.499<br><a href="summer.php"><button>Shop</button></a></div>
+<div class="list"><a href="velvet.php"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIxhO-CPReG2VEAy_0-WZqWRjAjkynAU6Jlg&s" alt=""></a>Velvet Bloom<br>Rs.499<br><a href="velvet.php"><button>Shop</button></a></div>
 </div>
+
+</body>
+</html>
