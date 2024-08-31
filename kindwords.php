4724b3c43852da73475f8501ea7fe2ff2284e7f1
Chouhanchetan <chouhanchetan114@gmail.com>
2024-08-31 15:29:48 +0530

    Add files via upload

---
+++ kindwords.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kindwords</title>
+    <link rel="stylesheet" href="kindwords.css">
+</head>
+<body>
+    <?php
+    include('nav.html');
+    ?>
+    <br>
+    <h1>KINDWORDS</h1>
+    <br>
+    <div class="container">
+        <div class="hello">
+        <img src="https://cdn.shopify.com/s/files/1/0588/1987/0876/files/unnamed_160x160.jpg?v=1655899914" alt="">
+        <div>
+            <p>"Been a fan of Beacon Bliss for a while now. I have used the soaps, the melts, the candles and their latest- The awesome whipped soaps and scrubs. My absolute favourite. This is the kind of extra baggage I don't mind lugging around wherever I go (exactly what I did when I switched countries). With my kind of extreme dry skin, these work wonderfully well. Can't wait for you guys to start shipping internationally." </p>
+            <p><b>- Nandini Biswas (Kansas City, U.S.A.)</b></p>
+</div>
+</div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="hello">
+        <img src="https://cdn.shopify.com/s/files/1/0588/1987/0876/files/Screenshot_2021-09-02_at_1.13.19_PM_160x160.png?v=1630568642" alt="">
+        <div>
+            <p>""Bathing has always been a key part of ‘me time’ in my life. That’s why it needs be more of a sensory experience rather than a just functional one. Batti & Co. provides that to the fullest! The artisanal, hand crafted bars are truly a treat to the senses. Can’t wait for some more ‘me time’!"" </p>
+            <p><b>- Aanam C (Mumbai)</b></p>
+</div>
+</div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="hello">
+        <img src="https://cdn.shopify.com/s/files/1/0588/1987/0876/files/WhatsApp_Image_2021-09-17_at_5.33.17_PM_1_160x160.jpg?v=1631959549" alt="">
+        <div>
+            <p>""Batti is my favourite bathing companion for 2 reasons- the person making these is awesome & each batti is yummy on the skin. Its feels gentle on the skin, it smells natural and just makes the entire bathing experience magical ❤️"" </p>
+            <p><b>- Swati Hans D'silva (Mumbai)</b></p>
+</div>
+</div>
+    </div>
+</body>
+</html>
